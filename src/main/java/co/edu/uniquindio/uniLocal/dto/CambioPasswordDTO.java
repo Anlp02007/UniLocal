622f02770588cc224e1214b5b50a304c36917018
Karen <jckarenmelissa@gmail.com>
2024-03-16 12:22:25 -0500

    sa agrega comentario de linea ne la clase CambioPasswordDTO

--- src/main/java/co/edu/uniquindio/uniLocal/dto/CambioPasswordDTO.java
+++ src/main/java/co/edu/uniquindio/uniLocal/dto/CambioPasswordDTO.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.uniLocal.dto;
 
 public record CambioPasswordDTO(
+        // atributos de la clas3e de cambioPassword K
         String passwordNueva,
         String id,
         String token
