e2f49d87a5a12010849223f56aacca21325dda69
Karen <jckarenmelissa@gmail.com>
2024-03-16 12:23:03 -0500

    se agrego comentario en de los atributos del cliente a la clase ActualizarClienteDTO

--- src/main/java/co/edu/uniquindio/uniLocal/dto/ActualizarClienteDTO.java
+++ src/main/java/co/edu/uniquindio/uniLocal/dto/ActualizarClienteDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record ActualizarClienteDTO (
+        //attributos de actualizar cliente M
         String id,
         @NotBlank @Length(max = 100) String nombre,
         String fotoPerfil,
